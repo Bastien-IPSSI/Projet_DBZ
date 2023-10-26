98f69197f9f744adc169436096a2758dc5bee9fe
trdmf1205 <121643862+trdmf1205@users.noreply.github.com>
2023-10-26 11:54:35 +0200

    Update main.php
    Mise en place des classes et de la méthode attaquer() avec choix d'attaque

--- main.php
+++ main.php
@@ -1,4 +1,116 @@
 <?php
 
+abstract class Personnage{
+    protected string $nom;
+    protected int $vie;
+    protected int $degats;
+    protected $pouvoir = array();
+
+    protected function __construct($nom,$vie,$degats){
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->degats = $degats;
+        $this->pouvoir = ["Coup de poing"];
+    }
+
+    // protected function attaquer();
+}
+
+class Gentil extends Personnage{
+    private int $niveau;
+    private int $experience;
+    private $pouvoirsDebloquer = array("Kamehaha","Genkidama");
+
+    public function __construct($nom,$vie,$degats){
+        parent::__construct($nom,$vie,$degats);
+        $this->niveau = 1;
+        $this->experience = 0;
+    }
+
+    // GETTERS
+    public function getNom(){
+        return $this->nom;
+    }
+    public function getVie(){
+        return $this->vie;
+    }
+    public function getDegats(){
+        return $this->degats;
+    }
+    public function getPouvoir(){
+        return $this->pouvoir;
+    }
+    public function getNiveau(){
+        return $this->niveau;
+    }
+    public function getExperience(){
+        return $this->experience;
+    }
+
+    // METHODS
+    public function choisirAttaque(){
+        $listePouvoirs = count($this->getPouvoir());
+        switch ($listePouvoirs) {
+            case 1:
+                $choix = (int)readline("Quelle attaque?\n1. Coup de poing");
+                while($choix !== 1){
+                    $choix = (int)readline("Quelle attaque?\n1. Coup de poing");
+                }
+                return $choix;
+                break;
+            case 2:
+                $choix = (int)readline("Quelle attaque?\n1. Coup de poing \n2. Kamehaha");
+                while($choix !== 1 or $choix !== 2){
+                    $choix = (int)readline("Quelle attaque?\n1. Coup de poing \n2. Kamehaha");
+                }
+                return $choix;
+            case 3:
+                $choix = (int)readline("Quelle attaque?\n1. Coup de poing \n2. Kamehaha \n3. Genkidama");
+                while($choix !== 1 or $choix !== 2 or $choix !== 3){
+                    $choix = (int)readline("Quelle attaque?\n1. Coup de poing \n2. Kamehaha \n3. Genkidama");
+                }
+                return $choix;
+        } 
+    }
+
+    public function attaquer($pouvoirChoisi){
+        switch ($pouvoirChoisi) {
+            case 1:
+                // COUP DE POING MULTIPLIE LES DEGATS INFLIGES PAR 1
+                return 1 * $this->getDegats();
+                break;
+            case 2:
+                // KAMEHAHA MULTIPLIE LES DEGATS INFLIGES PAR 2 ETC
+                return 2 * $this>getDegats();
+                break;
+            case 3:
+                // GENKIDAMA MULTIPLIE LES DEGATS INFLIGES PAR 2 ETC
+                return 3 * $this>getDegats();
+                break;
+    } 
+}}
+
+
+class Mechant extends Personnage{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    // GETTERS
+    public function getNom(){
+        return $this->nom;
+    }
+    public function getVie(){
+        return $this->vie;
+    }
+    public function getDegats(){
+        return $this->degats;
+    }
+}
+
+// RUN
+
+$goku = new Gentil("Goku", 10, 3);
+echo "il a tapé " . $goku->attaquer($goku->choisirAttaque());
 
 ?>
